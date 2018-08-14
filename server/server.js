ea3722e68c27af9e6fbb72b047428c151bb1c30e
shashank <shashanksmf@outlook.com>
2018-08-14 11:48:10 +0530

    removed connection

--- server/server.js
+++ server/server.js
@@ -41,8 +41,10 @@ boot(app, __dirname, function(err) {
 
 // server.listen(80);
 
-mongoose.connect('mongodb://admin:admin@cluster0-shard-00-00-wkiof.mongodb.net:27017,cluster0-shard-00-01-wkiof.mongodb.net:27017,cluster0-shard-00-02-wkiof.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true?authMode=scram-sha1/carinfo');
-// mongoose.connect('mongodb://localhost:27017/carinfo');
+// mongoose.connect('mongodb://admin:admin@cluster0-shard-00-00-wkiof.mongodb.net:27017,cluster0-shard-00-01-wkiof.mongodb.net:27017,cluster0-shard-00-02-wkiof.mongodb.net:27017/carinfo?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true?authMode=scram-sha1', {useNewUrlParser: false});
+
+// mongoose.connect('mongodb://admin:admin@cluster0-shard-00-00-wkiof.mongodb.net:27017,cluster0-shard-00-01-wkiof.mongodb.net:27017,cluster0-shard-00-02-wkiof.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true?authMode=scram-sha1/carinfonew');
+// mongoose.connect('mongodb://localhost:27017/carinfonew');
 
 // app.get('/', function (req, res) {
 //  res.sendFile(__dirname + '/index.html');
